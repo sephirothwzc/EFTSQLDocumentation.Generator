202bd2e7a052aa30e655ca78b51254cb9dee5ec8
wuzhanchao <wuzhanchao@BE27>
2017-03-11 21:42:07 +0800

    添加项目文件。

---
+++ edmx_form/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace edmx_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConnStr = new System.Windows.Forms.TextBox();
+            this.txtEdmxSrc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEdmxDes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtXmlnsNameSpace = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(393, 321);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 36);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "生成";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "数据库连接字符串：";
+            // 
+            // txtConnStr
+            // 
+            this.txtConnStr.Location = new System.Drawing.Point(293, 13);
+            this.txtConnStr.Multiline = true;
+            this.txtConnStr.Name = "txtConnStr";
+            this.txtConnStr.Size = new System.Drawing.Size(302, 75);
+            this.txtConnStr.TabIndex = 2;
+            this.txtConnStr.Text = "data source=60.205.94.121;initial catalog=HISSys;persist security info=True;user " +
+    "id=sa;password=wgzx@123;";
+            // 
+            // txtEdmxSrc
+            // 
+            this.txtEdmxSrc.Location = new System.Drawing.Point(293, 94);
+            this.txtEdmxSrc.Multiline = true;
+            this.txtEdmxSrc.Name = "txtEdmxSrc";
+            this.txtEdmxSrc.Size = new System.Drawing.Size(302, 49);
+            this.txtEdmxSrc.TabIndex = 4;
+            this.txtEdmxSrc.Text = "\\\\Mac\\Home\\Documents\\我的文件\\乱七八糟\\HIS\\SVN\\1. SOURCE\\HIS\\HIS\\Sephiroth.Ang.HIS.Entity" +
+    "s";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "源Edmx文件的物理路径：";
+            // 
+            // txtEdmxDes
+            // 
+            this.txtEdmxDes.Location = new System.Drawing.Point(293, 154);
+            this.txtEdmxDes.Multiline = true;
+            this.txtEdmxDes.Name = "txtEdmxDes";
+            this.txtEdmxDes.Size = new System.Drawing.Size(302, 49);
+            this.txtEdmxDes.TabIndex = 6;
+            this.txtEdmxDes.Text = "\\\\Mac\\Home\\Documents\\我的文件\\乱七八糟\\HIS\\SVN\\1. SOURCE\\HIS\\HIS\\Sephiroth.Ang.HIS.Entity" +
+    "s";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "新生成Edmx文件的物理路径：";
+            // 
+            // txtXmlnsNameSpace
+            // 
+            this.txtXmlnsNameSpace.Location = new System.Drawing.Point(293, 217);
+            this.txtXmlnsNameSpace.Multiline = true;
+            this.txtXmlnsNameSpace.Name = "txtXmlnsNameSpace";
+            this.txtXmlnsNameSpace.Size = new System.Drawing.Size(302, 63);
+            this.txtXmlnsNameSpace.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(275, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Edmx中的xmlns命名空间（每个版本可能不一样）：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 421);
+            this.Controls.Add(this.txtXmlnsNameSpace);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEdmxDes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEdmxSrc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtConnStr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "EF映射添加注释工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtConnStr;
+        private System.Windows.Forms.TextBox txtEdmxSrc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEdmxDes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtXmlnsNameSpace;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ edmx_form/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.IO;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace edmx_form
+{
+    public partial class Form1 : Form
+    {
+        private NameValueCollection _AppSettings;
+        /// <summary>AppSettings</summary>
+        public NameValueCollection AppSettings { get { return _AppSettings ?? (_AppSettings = ConfigurationManager.AppSettings); } }
+
+        private ConnectionStringSettingsCollection _ConnectionStrings;
+        /// <summary>连接字符串设置</summary>
+        public ConnectionStringSettingsCollection ConnectionStrings { get { return _ConnectionStrings ?? (_ConnectionStrings = ConfigurationManager.ConnectionStrings); } }
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (AppSettings != null && AppSettings.Count > 0)
+            {
+                //txtEdmxSrc.Text = AppSettings["InputFileName"];
+                //txtEdmxDes.Text = AppSettings["OutputFileName"];
+                txtXmlnsNameSpace.Text = AppSettings["XmlnsNameSpaces"];
+            }
+            if (ConnectionStrings != null && ConnectionStrings.Count > 0)
+            {
+               // txtConnStr.Text = ConnectionStrings["ConnStr"] != null ? ConnectionStrings["ConnStr"].ConnectionString : null;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            String connStr = txtConnStr.Text.Trim();
+            String inputFileName = txtEdmxSrc.Text.Trim();
+            String outputFileName = txtEdmxDes.Text.Trim();
+            String xmlnsNameSpace = txtXmlnsNameSpace.Text.Trim();
+            if (String.IsNullOrEmpty(connStr))
+            {
+                MessageBox.Show("请填写数据库连接字符串！");
+                return;
+            }
+            if (String.IsNullOrEmpty(inputFileName))
+            {
+                MessageBox.Show("请填写源Edmx文件的物理路径！");
+                return;
+            }
+            if (String.IsNullOrEmpty(outputFileName))
+            {
+                MessageBox.Show("请填写新生成Edmx文件的物理路径！");
+                return;
+            }
+            if (String.IsNullOrEmpty(xmlnsNameSpace))
+            {
+                MessageBox.Show("请填写Edmx中的xmlns命名空间（每个版本可能不一样）！");
+                return;
+            }
+            try
+            {
+                var creater = new Creater(connStr, inputFileName, outputFileName, xmlnsNameSpace);
+                creater.CreateDocumentation();
+                creater.Dispose();
+                MessageBox.Show("宾果，操作成功！");
+                //this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("异常：" + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    /// <summary>修改数据库映射到EF edmx文件，为其添加数据库表、列注释</summary>
+    public class Creater : IDisposable
+    {
+        private String _ConnStr;
+        /// <summary>数据库连接字符串</summary>
+        public String ConnStr { get { return _ConnStr; } set { _ConnStr = value; } }
+
+        private String _InputFileName;
+        /// <summary>输入文件（绝对路径）</summary>
+        public String InputFileName { get { return _InputFileName; } set { _InputFileName = value; } }
+
+        private String _OutputFileName;
+        /// <summary>输出文件（绝对路径）</summary>
+        public String OutputFileName { get { return _OutputFileName; } set { _OutputFileName = value; } }
+
+        private SqlConnection _Conn;
+        /// <summary>数据库连接</summary>
+        public SqlConnection Conn { get { return _Conn; } set { _Conn = value; } }
+
+        private String _XmlnsNameSpace;
+        /// <summary>Xmlns命名空间</summary>
+        public String XmlnsNameSpace { get { return _XmlnsNameSpace; } set { _XmlnsNameSpace = value; } }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="connStr"></param>
+        /// <param name="inputFileName"></param>
+        /// <param name="outputFileName"></param>
+        /// <param name="xmlnsNameSpace"></param>
+        public Creater(String connStr, String inputFileName, String outputFileName, String xmlnsNameSpace)
+        {
+            ConnStr = connStr;
+            InputFileName = inputFileName;
+            OutputFileName = outputFileName;
+            XmlnsNameSpace = xmlnsNameSpace;
+            Conn = new SqlConnection(connStr);
+        }
+
+        public void Dispose()
+        {
+            Conn.Dispose();
+        }
+        /// <summary>创建注释文本</summary>
+        public void CreateDocumentation()
+        {
+            Conn.Open();
+            var doc = XDocument.Load(InputFileName);
+            var entityTypeElements = doc.Descendants("{" + XmlnsNameSpace + "}EntityType");
+            foreach (var entityTypeElement in entityTypeElements)
+            {
+                var tableName = entityTypeElement.Attribute("Name").Value;
+                var propertyElements = entityTypeElement.Descendants("{" + XmlnsNameSpace + "}Property");
+                AddNodeDocumentation(entityTypeElement, GetTableDocumentation(tableName));
+                foreach (var propertyElement in propertyElements)
+                {
+                    var columnName = propertyElement.Attribute("Name").Value;
+                    AddNodeDocumentation(propertyElement, GetColumnDocumentation(tableName, columnName));
+                }
+            }
+            if (File.Exists(OutputFileName))
+            {
+                File.Delete(OutputFileName);
+            }
+            doc.Save(OutputFileName);
+        }
+        /// <summary>
+        /// 给指定的Xml节点添加文档说明
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="documentation"></param>
+        private void AddNodeDocumentation(XElement element, String documentation)
+        {
+            if (String.IsNullOrEmpty(documentation))
+            {
+                return;
+            }
+            element.Descendants("{" + XmlnsNameSpace + "}Documentation").Remove();
+            element.AddFirst(new XElement("{" + XmlnsNameSpace + "}Documentation", new XElement("{" + XmlnsNameSpace + "}Summary", documentation)));
+        }
+
+        /// <summary>获取表文档描述</summary>
+        /// <param name="tableName">表名</param>
+        /// <returns></returns>
+        private String GetTableDocumentation(String tableName)
+        {
+            using (var command = new SqlCommand(@"SELECT [value] 
+                                                          FROM fn_listextendedproperty (
+                                                                'MS_Description', 
+                                                                'schema', 'dbo', 
+                                                                'table',  @TableName, 
+                                                                null, null)", Conn))
+            {
+                command.Parameters.AddWithValue("@TableName", tableName);
+                return command.ExecuteScalar() as String;
+            }
+        }
+        /// <summary>
+        /// 获取指定表的指定列文档描述
+        /// </summary>
+        /// <param name="tableName">指定表</param>
+        /// <param name="columnName">指定列</param>
+        /// <returns></returns>
+        private String GetColumnDocumentation(String tableName, String columnName)
+        {
+            using (SqlCommand command = new SqlCommand(@"SELECT [value] 
+                                                         FROM fn_listextendedproperty (
+                                                                'MS_Description', 
+                                                                'schema', 'dbo', 
+                                                                'table', @TableName, 
+                                                                'column', @columnName)", Conn))
+            {
+
+                command.Parameters.AddWithValue("TableName", tableName);
+                command.Parameters.AddWithValue("ColumnName", columnName);
+
+                return command.ExecuteScalar() as String;
+            }
+        }
+    }
+}
